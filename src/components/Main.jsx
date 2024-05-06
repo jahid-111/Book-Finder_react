fb3ba94cfdc1c2f744133e0689a0fd962b39ded2
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-05-06 04:55:13 +0300

    Formatting-Display

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Header from './Header';
+import Books from './books/Books';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Books></Books>
+        </div>
+    );
+};
+
+export default Main;
